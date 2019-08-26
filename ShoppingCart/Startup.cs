05b5206350279df3fe8c87e9b47c45947cc445cf
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-08-25 23:54:50 -0400

    started work on the ShoppingCart Service/Module

--- ShoppingCart/Startup.cs
+++ ShoppingCart/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Nancy;
+using Nancy.Configuration;
+using Nancy.Owin;
 
 namespace ShoppingCart
 {
@@ -20,12 +23,7 @@ namespace ShoppingCart
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
+            app.UseOwin(buildFunc => buildFunc.UseNancy());
         }
     }
 }
