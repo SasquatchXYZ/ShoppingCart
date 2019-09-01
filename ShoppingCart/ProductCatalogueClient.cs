8fc18b5a8216db35b45c28125b7ecc17113454da
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-09-01 18:07:11 -0400

    finished up the productCatalogueClient

--- ShoppingCart/ProductCatalogueClient.cs
+++ ShoppingCart/ProductCatalogueClient.cs
@@ -6,16 +6,35 @@ using System.Net.Http;
 using System.Threading;
 using Newtonsoft.Json;
 using Polly;
+using Polly.Retry;
 using ShoppingCart;
 
 namespace ShoppingCart
 {
     public class ProductCatalogueClient : IProductCatalogueClient
     {
+        private static AsyncRetryPolicy exponentialRetryPolicy =
+            Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(
+                    3, attempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, attempt))
+                );
+
         private static string productCatalogBaseUrl =
             @"http://private-05cc8-chapter2productcatalogmicroservice.apiary-mock.com";
 
         private static string getProductPathTemplate = "/products?productIds=[{0}]";
+
+        public async Task<IEnumerable<ShoppingCartItem>> GetShoppingCartItems(int[] productCatalogIds) =>
+            exponentialRetryPolicy.ExecuteAsync(async () =>
+                await GetItemsFromCatalogueService(productCatalogIds)
+                    .ConfigureAwait(false));
+
+        private static async Task<IEnumerable<ShoppingCartItem>> GetItemsFromCatalogueService(int[] productCatalogueIds)
+        {
+            var response = await RequestProductFromProductCatalogue(productCatalogueIds).ConfigureAwait(false);
+            return await ConvertToShoppingCartItems(response).ConfigureAwait(false);
+        }
 
         private static async Task<HttpResponseMessage> RequestProductFromProductCatalogue(int[] productCatalogueIds)
         {
